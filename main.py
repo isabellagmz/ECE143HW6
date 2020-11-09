8eac4fb67dd83d922cccbcac0b54ebdf986e5d0a
isabellagomez <ilgomezt@ucsd.edu>
2020-11-08 16:59:21 -0800

    TRY 1. multinomial.py does what i think it should do

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # Isabella Gomez A15305555
 # ECE143 HW6
 
+from multinomial import multinomial_sample
 class Homework6:
     def __init__(self):
         pass
 
 if __name__ == '__main__':
     my_Homework6 = Homework6()
-
+    print(multinomial_sample(10,[1/2,1/2],k=10))
--- multinomial.py
+++ multinomial.py
@@ -1,3 +1,73 @@
 # Isabella Gomez A15305555
 # ECE143 HW6
 
+import random
+from random import randint
+from random import seed
+def multinomial_sample(n,p,k=1):
+    '''
+    This function
+
+    :param n: number of trials
+    :param p: list of probabilities
+    :param k: number of desired samples
+    :return: samples from a multinomial distribution
+    '''
+
+    # check that k and n are ints
+    assert type(n) == int
+    assert type(k) == int
+
+    # check p is list
+    assert type(p) == list
+
+    # check that the addition of numbers in p is 1
+    probs=0
+    for num in range(len(p)):
+        probs = probs + p[num]
+    assert probs == 1
+
+    # check that K >= len(p) or it won't be able to be divided
+    assert k >= len(p)
+
+    multinomial = []
+    number_of_divisions = len(p)
+
+    # run experiments for n number of trials
+    for trial in range(n):
+        entry = []
+        # if k = len(p) populate multinomial with 1
+        if k == len(p):
+            for i in range(len(p)):
+                entry.append(1)
+        # if probability is 1
+        elif len(p) == 1:
+            entry.append(k)
+        else:
+            # generate a random number between 1 and k - len(p)
+            rand_int = randint(1, k - len(p) + 1)
+            entry.append(rand_int)
+            # find the remaining difference
+            difference = k - rand_int -1
+
+            for i in range(len(p)-1):
+                # check that the numbers in list are equal to k
+                if i == len(p)-2:
+                    sum = 0
+                    for j in range(len(entry)):
+                        sum = sum + entry[j]
+                    last_num = k - sum
+                    entry.append(last_num)
+                    break
+                # check that value of k has not been reached
+                if difference <= 0:
+                    entry.append(1)
+                    break
+                # append next random number
+                rand_int = randint(1, difference)
+                entry.append(rand_int)
+                difference = difference - rand_int
+        # append entry to multinomial list
+        multinomial.append(entry)
+
+    return multinomial
