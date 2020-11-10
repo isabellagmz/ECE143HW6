265a1aff226f010c35116c1e3a46909b74844c18
isabellagomez <ilgomezt@ucsd.edu>
2020-11-10 15:24:53 -0800

    encrypted.py has encryption working

--- encrypted.py
+++ encrypted.py
@@ -1,2 +1,80 @@
 # Isabella Gomez A15305555
 # ECE143 HW6
+
+import os
+from random import randint
+def encrypt_message(message,fname):
+    '''
+    Given `message`, which is a lowercase string without any punctuation, and `fname` which is the
+    name of a text file source for the codebook, generate a sequence of 2-tuples that
+    represents the `(line number, word number)` of each word in the message. The output is a list
+    of 2-tuples for the entire message. Repeated words in the message should not have the same 2-tuple.
+
+    :param message: message to encrypt
+    :param fname: filename for source text
+    :return: list of 2-tuples
+    '''
+
+    # check that filename and message are text
+    assert type(fname) == str
+    assert type(message) == str
+
+    # check that file exists
+    assert os.path.isfile(fname)
+
+    # open the file
+    file = open(fname, 'r')
+
+    # store the lines of the text in a list
+    list_of_lines = []
+    for i, lines in enumerate(file):
+        list_of_lines.append(lines)
+
+    list_of_words = []
+    for i in range(len(list_of_lines)):
+        word = list_of_lines[i].split()
+        # remove punctuation and make lowercase
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        for j in range(len(word)):
+            word[j] = word[j].lower()
+            for char in word[j]:
+                if char in punctuations:
+                    word[j] = word[j].replace(char, "")
+        list_of_words.append(word)
+
+    # make a list of words from the message
+    message_list = []
+    for word in message.split():
+        message_list.append(word)
+
+    final_list = []
+    temp = []
+    # check if the word is in the line
+    for k in range(len(message_list)):
+        for i in range(len(list_of_words)):
+            for j in range(len(list_of_words[i])):
+                if message_list[k] == list_of_words[i][j]:
+                    temp.append((i,j))
+        # generating a random number
+        rand_int = randint(0,len(temp)-1)
+        final_list.append(temp[rand_int])
+        # empty temp
+        temp = []
+
+    # check that every element in final_list is unique
+    assert len(final_list) == len(set(final_list))
+
+    return final_list
+
+def decrypt_message(inlist,fname):
+    '''
+    Given `inlist`, which is a list of 2-tuples`fname` which is the
+    name of a text file source for the codebook, return the encrypted message.
+
+    :param inlist: inlist to decrypt
+    :param fname: filename for source text
+    :return: string decrypted message
+    '''
+
+
+    pass
--- main.py
+++ main.py
@@ -3,11 +3,16 @@
 
 from multinomial import multinomial_sample
 from chunkify import split_by_n
+from encrypted import encrypt_message
+from encrypted import decrypt_message
 class Homework6:
     def __init__(self):
         pass
 
 if __name__ == '__main__':
     my_Homework6 = Homework6()
+    fname = "/Users/isabellagomez/Documents/ECE143/metamorphosis.txt"
     # print(multinomial_sample(10,[1/3,1/3,1/3],k=10))
-    print(split_by_n("/Users/isabellagomez/Documents/ECE143/metamorphosis.txt", 8))
+    # print(split_by_n("/Users/isabellagomez/Documents/ECE143/metamorphosis.txt", 8))
+    message ="let us not say we met late at the night about the secret"
+    print(encrypt_message(message,fname))
